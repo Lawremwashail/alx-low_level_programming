6d45eb64fcd8426d7a6c04e233d5a3f625af9a48
Lawremwashail <lawremwash@gmail.com>
2023-10-31 11:32:34 +0000

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,54 +10,54 @@
 int main(int argc, char **argv)
 {
 	int file_to, file_from, fd_to, fd_from;
-	char *buff;
+	char *buffer;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buff = malloc(sizeof(char) * BUF_SIZE);
-	if (!buff)
+	buffer = malloc(sizeof(char) * BUF_SIZE);
+	if (!buffer)
 	{
 		return (0);
 	}
-	file_from = open(argv[1], O_RDONLY);
-	error_98(file_from, buff, argv[1]);
+	file_from = open(argv[2], O_RDONLY);
+	error_98(file_from, buffer, argv[2]);
 	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	error_99(file_to, buff, argv[2]);
+	error_99(file_to, buffer, argv[2]);
 	do {
-		fd_to = read(file_from, buff, BUF_SIZE);
+		fd_to = read(file_from, buffer, BUF_SIZE);
 		if (fd_to == 0)
 		{
 			break;
 		}
-		error_98(fd_to, buff, argv[1]);
-		fd_from = write(file_to, buff, fd_to);
-		error_99(fd_from, buff, argv[2]);
+		error_98(fd_to, buffer, argv[1]);
+		fd_from = write(file_to, buffer, fd_to);
+		error_99(fd_from, buffer, argv[2]);
 	} while (fd_from >= BUF_SIZE);
 	fd_to = close(file_to);
-	error_100(fd_to, buff);
+	error_100(fd_to, buffer);
 	fd_to = close(file_from);
-	error_100(fd_to, buff);
-	free(buff);
+	error_100(fd_to, buffer);
+	free(buffer);
 	return (0);
 }
 
 /**
 * error_98 - function to check error 98 from main function
 * @file_to: the file to check
-* @buff: the buffer
+* @buffer: the buffer
 * @argv: pointer to argument array
 * Description: prints read file error
 */
 
-void error_98(int file_to, char *buff, char *argv)
+void error_98(int file_to, char *buffer, char *argv)
 {
 	if (file_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv);
-		free(buff);
+		free(buffer);
 		exit(98);
 	}
 }
@@ -65,36 +65,34 @@ void error_98(int file_to, char *buff, char *argv)
 /**
 * error_99 - function to check error 99 from main function
 * @file_to: the file to check
-* @buff: the buffer
+* @buffer: the buffer
 * @argv: pointer to argument array
 * Description: prints write file error
 */
 
-void error_99(int file_to, char *buff, char *argv)
+void error_99(int file_to, char *buffer, char *argv)
 {
 	if (file_to < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: cant't write to %s\n", argv)
-		{
-			free(buff);
-			exit(99);
-		}
+		dprintf(STDERR_FILENO, "Error: cant't write to %s\n", argv);
+		free(buffer);
+		exit(99);
 	}
 }
 
 /**
 * error_100 - function to check error 100 from main function
 * @file_to: the file to check
-* @buff: the buffer
+* @buffer: the buffer
 * Description: prnts close file error
 */
 
-void error_100(int file_to, char *buff)
+void error_100(int file_to, char *buffer)
 {
 	if (file_to < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %i\n", file_to)
-			free(buff);
-			exit(100);
+		dprintf(STDERR_FILENO, "Error: can't close fd %i\n", file_to);
+		free(buffer);
+		exit(100);
 	}
 }
